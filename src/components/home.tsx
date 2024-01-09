9ee4e7d90f1c72a1b688a27c5b28be2e0ffc777f
mgns420 <magnus.brix@gmx.de>
2024-01-09 20:41:51 +0100

    home erster Entwurf

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import "./css/navbar.css";
-import { Link } from 'react-router-dom';
+import "./css/home.css";
+import { Link, useNavigate } from 'react-router-dom';
 
 const Home = () => {
 
+    const navigate = useNavigate();
+
+    const onClickLogin = () => {
+        navigate("/login");
+    }
+
+    const onClickRegist = () => {
+        navigate("/regist");
+    }
+
     return (
         <>  
-            <div>
-                <p>Home</p>
+            <div className="wrapper">
+
+                <div className="headerDiv">
+                    <h1 className="header">Spaß mit Keyboards!!</h1>
+                </div>
+
+                <div className="form">
+
+                    <div className="login">
+                        <h2>Meld dich jetzt an!</h2>
+                        <button onClick={onClickLogin}>Login</button>
+                    </div>
+
+                    <div className="regist">
+                        <h2>Werd Teil der</h2>
+                        <h2>Keyboard-Gang!</h2>
+                        <button onClick={onClickRegist}>Sign Up!</button>
+                    </div>
+
+                </div>
+
             </div>
         </>
     );
