ac4d0be664f4e3ce590f08a6f6ee07a40a93d9ea
marcushsv <115557369+marcushsv@users.noreply.github.com>
2024-01-22 11:24:16 +0100

    versuch test fetchdata

--- src/tests/fetchedUserdata.test.tsx
+++ src/tests/fetchedUserdata.test.tsx
@@ -10,14 +10,6 @@ import {
 } from "../components/api";
 import { User } from "@prisma/client";
 
-test("fetchTest", async () => {
-  const points = await useFetchPoints("/points/leaderboard/1");
-
-  global.fetch = jest.fn().mockResolvedValue({
-    ok: true,
-    json: jest.fn().mockResolvedValue(expectedData),
-  });
-
-  const pointsId = points?.[0].id;
-  expect(pointsId).toBe(10);
-});
+// test("fetchTest", async () => {
+//   await expect(useFetchUserdata())
+// });
