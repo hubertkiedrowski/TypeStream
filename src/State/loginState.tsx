0a4122fd08c9e1f3e5d485083abdd968c83f48d0
mgns420 <magnus.brix@gmx.de>
2024-01-26 17:07:13 +0100

    geschützte Routen mit Globalen State aber einem bug

--- src/State/loginState.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState }  from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-
-const LoginState = () => {
-
-
-
-}
--- src/components/helper/redirect.ts
+++
@@ -1,8 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-const handleRedirect = (path: string) => {
-  const navigate = useNavigate();
-  navigate(path);
-};
-
-export default handleRedirect;
